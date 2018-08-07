8b5536b596672c9deb44f7f4eea89fb1f9324dc2
Keita-Kuki <k.ta.2321@gmail.com>
2018-08-07 22:02:10 +0900

    add assets

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-def main(video_file_path, audio_file_path, subtitles_file_path):
-    
+import cv2
+
+def main(video_file_name, audio_file_name, subtitles_file_name):
+    video = cv2.VideoCapture(video_file_name)
+    audio =
+    subtitles = subtitle_parser()
+
+    speaker = speaker_detection()
+    subtitle_placement(speaker, )
+
+
+def speaker_detection(video_file_path, audio_file_path, subtitles_file_path):
+
+def subtitle_placement():
+
