94f3b0209c0dca1c9db3cf13ce0cef00578e51bb
Keita-Kuki <k.ta.2321@gmail.com>
2018-08-08 10:50:55 +0900

    MSD

--- main.py
+++ main.py
@@ -1,15 +1,67 @@
 import cv2
+import sys
+import soundfile
+from dateutil.parser import parse
+
+import speaker_detector as sd
+# import subtitle_placement_optimizer
+
+# parameters
+theta1 = 20
+theta2 = 2.5
+theta3 = 2
+theta4 = 0.1
+theta5 = 2
 
 def main(video_file_name, audio_file_name, subtitles_file_name):
-    video = cv2.VideoCapture(video_file_name)
-    audio = 
-    subtitles = subtitle_parser()
+    audio, sampling_rate = soundfile.read(audio_file_name)
+    subtitle_start_time, subtitle_end_time, subtitle = subtitle_parser(subtitles_file_name)
+    speaker = speaker_detection(video_file_name, audio, subtitle, subtitle_start_time, subtitle_end_time)
+    print(speaker)
+    subtitle_placement(speaker)
 
-    speaker = speaker_detection()
-    subtitle_placement(speaker, )
+# 字幕ファイルから時間と字幕を抜き出す関数
+def subtitle_parser(subtitles_file_name):
+    srt_string = open(subtitles_file_name).read()
+    lines = srt_string.splitlines()
+    subtitle = ""
+    preline_status = 0  # 0: blank, 1: number, 2:timestamp, 3:subtitle
+    for line in lines:
+        if line == "":
+            preline_status = 0
+            continue
+        if preline_status == 0:
+            preline_status = 1
+        elif preline_status == 1:
+            start_time = parse(line.split()[0])
+            end_time = parse(line.split()[-1])
+            preline_status = 2
+        elif preline_status == 2 or preline_status == 3:
+            subtitle += line
+            preline_status = 3
+    return start_time, end_time, subtitle
+
+# 論文のアルゴリスム1部分
+def speaker_detection(video_file_name, audio, subtitle, subtitle_start_time, subtitle_end_time):
+    faces = sd.detect_faces(video_file_name)
+    msds = sd.calc_msds(faces)
+    faces, msds = sd.filter_by_msd(faces, msds, theta1)
+    if len(faces) >= 2:
+        faces, msds = sd.filter_by_msd(faces, msds, max(msds)/theta2)
 
 
-def speaker_detection(video_file_path, audio_file_path, subtitles_file_path):
+
+    if len(faces) == 1:
+        return faces[0]
+    else:
+        return []
+
+
+
 
 def subtitle_placement():
+    pass
 
+if __name__ == "__main__":
+    args = sys.argv
+    main(args[1], args[2], args[3])
---
+++ speaker_detector.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+
+cascade_file_name = "assets/haarcascade_frontalface_default.xml"
+cascade = cv2.CascadeClassifier(cascade_file_name)
+
+
+def load_video(video_file_name):
+    video = cv2.VideoCapture(video_file_name)
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    return video, frame_count
+
+
+def detect_faces(video_file_name):
+    avg_img = generate_avg_img(video_file_name)
+    faces = detect_faces_from_an_img(avg_img)
+    return faces
+
+
+def generate_avg_img(video_file_name):
+    video, frame_count = load_video(video_file_name)
+    avg_img = np.zeros((720, 1280, 3))
+    for i in range(frame_count):
+        _, img = video.read()
+        try:
+            avg_img += img
+        except TypeError:
+            pass
+    avg_img = avg_img / frame_count
+    cv2.imwrite(f'assets/avg.jpg', avg_img)
+    return cv2.imread(f'assets/avg.jpg')
+
+
+def detect_faces_from_an_img(avg_img):
+    faces = cascade.detectMultiScale(avg_img)
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(avg_img, (x, y), (x + w, y + h), (0, 0, 200), 3)
+    # cv2.imwrite(f'../assets/test_avg.jpg', avg_img)
+    try:
+        return faces.tolist()
+    except AttributeError:
+        return []
+
+def calc_msds(video_file_name, faces):
+    msds = []
+    for face in faces:
+        msds.append(calc_msd(face, video_file_name))
+    return msds
+
+
+def calc_msd(face, video_file_name):
+    video, frame_count = load_video(video_file_name)
+    x, y, w, h = estimate_mouse(face)
+    mouse_im = []
+    msd = 0
+    for i in range(frame_count):
+        _, img = video.read()
+        try:
+            mouse_i = img[y:y + h, x:x + w].astype("int16")
+            if len(mouse_im) != 0:
+                msd += ((mouse_i - mouse_im) ** 2).sum() / h / w
+        except TypeError:
+            pass
+        mouse_im = mouse_i
+    msd = msd / (frame_count - 1)
+    return msd
+
+
+def estimate_mouse(face):
+    x, y, w, h = face
+    return int(x + w / 4), int(y + h * 3 / 4), int(x + w * 3 / 4), int(y + h)
+
+
+def filter_by_msd(faces, msds, threshold):
+    faces_, msds_ = []
+    for face, msd in zip(faces, msds):
+        if msd > threshold:
+            faces_.append(face)
+            msds_.append(msd)
+    return faces_, msds_
